5ba4d4c94391b26c3e7263e92196175ee2d0e624
Misha <netesam7@gmail.com>
2023-06-10 20:01:54 +0300

    add pleyer, enemy, finish

--- main.py
+++ main.py
@@ -19,6 +19,22 @@ fon = pygame.transform.scale(fon, (WIN_WIDTH, WIN_HEIGHT))
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 clock = pygame.time.Clock()
 
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self ,x , y, width, height, image):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.image.load(file_path(image))
+        self.image = pygame.transform.scale(self.image, (width, height))
+
+    def show(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+player = GameSprite(450, 0, 100, 100, r"images\pngwing.com (6).png" )
+enemy = GameSprite(450, 240, 70, 70, r"images\Peg-Leg_Pete.png" )
+finish =  GameSprite(500, 405, 50, 35, r"images\crystal.png" )
+
+
+
+
 level = 1
 game = True
 
@@ -29,6 +45,10 @@ while game:
 
     if level == 1:
         window.blit(fon, (0, 0))
+        player.show()
+        enemy.show()
+        finish.show()
+
 
     clock.tick(FPS)
     pygame.display.update()
